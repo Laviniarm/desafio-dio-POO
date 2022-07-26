5d66d6242ee8c30478e00433473c2c59b2ad48ad
Lavínia Rodrigues <lavinia.rodriguesr@gmail.com>
2022-07-26 13:45:03 -0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -8,20 +10,56 @@ public class Main {
         Curso curso1 = new Curso();
         curso1.setTitulo("Curso java");
         curso1.setDescricao("Descrição curso java");
-        curso1.setCagaHoraria(8);
+        curso1.setCargaHoraria(8);
 
         Curso curso2 = new Curso();
         curso2.setTitulo("Curso java");
         curso2.setDescricao("Descrição curso java");
-        curso2.setCagaHoraria(8);
+        curso2.setCargaHoraria(4);
 
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria em java");
         mentoria.setDescricao("Descrição mentoria em java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devLavinia = new Dev();
+        devLavinia.setNome("Lavínia");
+        devLavinia.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Lavínia:" + devLavinia.getConteudosInscritos());
+        devLavinia.progredir();
+        devLavinia.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Lavínia:" + devLavinia.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Lavínia:" + devLavinia.getConteudosConcluidos());
+        System.out.println("XP:" + devLavinia.calcularTotalXp());
+
+
+        System.out.println("------------");
+
+        Dev devMateus = new Dev();
+        devMateus.setNome("Mateus");
+        devMateus.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Mateus:" + devMateus.getConteudosInscritos());
+        devMateus.progredir();
+        devMateus.progredir();
+        devMateus.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Mateus:" + devMateus.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Mateus:" + devMateus.getConteudosConcluidos());
+        System.out.println("XP:" + devMateus.calcularTotalXp());
+
+
+
     }
 }
--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -2,25 +2,17 @@ package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
-    String titulo;
-    String descricao;
-    LocalDate data;
+public class Mentoria extends Conteudo {
 
-    public String getTitulo() {
-        return titulo;
+    private LocalDate data;
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + 20d;
     }
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
+    public Mentoria(){
 
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
     }
 
     public LocalDate getData() {
@@ -34,9 +26,10 @@ public class Mentoria {
     @Override
     public String toString() {
         return "Mentoria{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", data=" + data +
                 '}';
     }
+
 }
